e1536f00a64df29a8f0eae1dfc2fbe06159645f9
alicelukas <alicelukas@naver.com>
2018-12-25 17:11:06 +0900

    contentful 연결? 우선 저장

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,6 @@
+// ./nuxt.config.js
+const config = require('./contentful.json')
+
 module.exports = {
   /*
   ** Headers of the page
@@ -34,6 +37,12 @@ module.exports = {
         })
       }
     }
+  },
+  env: {
+    CTF_SPACE_ID: config.CTF_SPACE_ID,
+    CTF_CDA_ACCESS_TOKEN: config.CTF_CDA_ACCESS_TOKEN,
+    CTF_PERSON_ID: config.CTF_PERSON_ID,
+    CTF_BLOG_POST_TYPE_ID: config.CTF_BLOG_POST_TYPE_ID
   }
 }
 
--- pages/index.vue
+++ pages/index.vue
@@ -19,13 +19,50 @@
           class="button--grey">GitHub</a>
       </div>
     </div>
+
+    <div>
+      <!-- render data of the person -->
+      <h1>{{ person.fields.name }}</h1>
+      <!-- render blog posts -->
+      <ul>
+        <li v-for="(post, index) in posts" :key="index">
+          {{ post.fields.title }}
+        </li>
+      </ul>
+    </div>
   </section>
 </template>
 
 <script>
 import AppLogo from '~/components/AppLogo.vue'
+import {createClient} from '~/plugins/contentful.js'
+
+const client = createClient()
 
 export default {
+  // `env` is available in the context object
+  asyncData ({env}) {
+    return Promise.all([
+      // fetch the owner of the blog
+      client.getEntries({
+        'sys.id': env.CTF_PERSON_ID
+      }),
+      // fetch all blog posts sorted by creation date
+      client.getEntries({
+        'content_type': env.CTF_BLOG_POST_TYPE_ID,
+        order: '-sys.createdAt'
+      })
+    ]).then(([entries, posts]) => {
+      // return data that should be available
+      // in the template
+      console.log('aslfkj: ', entries);
+      console.log('aslfkj: ', posts);
+      return {
+        person: entries.items[0],
+        posts: posts.items
+      }
+    }).catch(console.error)
+  },
   components: {
     AppLogo
   }
